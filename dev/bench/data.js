ad4a935ac5e8142cb78e5574f8423d83888d91fe
github-action-benchmark <github@users.noreply.github.com>
2020-12-11 10:41:16 +0000

    add pipeline (cargo) benchmark result for b4e24332a0cd44068a806081d51686f50c086056

---
+++ dev/bench/data.js
@@ -0,0 +1,38 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1607683275605,
+  "repoUrl": "https://github.com/ernestas-poskus/hyper",
+  "entries": {
+    "pipeline": [
+      {
+        "commit": {
+          "author": {
+            "email": "sean@seanmonstar.com",
+            "name": "Sean McArthur",
+            "username": "seanmonstar"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "b4e24332a0cd44068a806081d51686f50c086056",
+          "message": "feat(client): change DNS Resolver to resolve to SocketAddrs (#2346)\n\nThe DNS resolver part of `HttpConnector` used to require resolving to\r\n`IpAddr`s, and this changes it so that they resolve to `SocketAddr`s.\r\nThe main benefit here is allowing for resolvers to set the IPv6 zone ID\r\nwhen resolving, but it also just more closely matches\r\n`std::net::ToSocketAddrs`.\r\n\r\nCloses #1937\r\n\r\nBREAKING CHANGE: Custom resolvers used with `HttpConnector` must change\r\n  to resolving to an iterator of `SocketAddr`s instead of `IpAddr`s.",
+          "timestamp": "2020-12-03T14:21:19-08:00",
+          "tree_id": "f15bc5873b96ec18a2c130878bec5136d305e561",
+          "url": "https://github.com/ernestas-poskus/hyper/commit/b4e24332a0cd44068a806081d51686f50c086056"
+        },
+        "date": 1607683273718,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "hello_world_16",
+            "value": 49228,
+            "range": "± 5664",
+            "unit": "ns/iter"
+          }
+        ]
+      }
+    ]
+  }
+}
