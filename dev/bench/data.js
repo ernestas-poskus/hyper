efff2104d3adac0646159938d7622b3802967eca
github-action-benchmark <github@users.noreply.github.com>
2021-10-10 20:43:05 +0000

    add connect (cargo) benchmark result for 6169db250c932dd012d391389826cd34833077b4

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1633898574527,
+  "lastUpdate": 1633898584618,
   "repoUrl": "https://github.com/ernestas-poskus/hyper",
   "entries": {
     "pipeline": [
@@ -391,6 +391,36 @@ window.BENCHMARK_DATA = {
             "name": "http_connector",
             "value": 74538,
             "range": "± 8896",
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "nox@nox.paris",
+            "name": "Anthony Ramine",
+            "username": "nox"
+          },
+          "committer": {
+            "email": "sean@seanmonstar.com",
+            "name": "Sean McArthur",
+            "username": "seanmonstar"
+          },
+          "distinct": true,
+          "id": "6169db250c932dd012d391389826cd34833077b4",
+          "message": "feat(h2): always include original h2 error on broken pipe",
+          "timestamp": "2021-10-04T13:35:07-07:00",
+          "tree_id": "e55c996e68297d08a6630e792941b468bd581612",
+          "url": "https://github.com/ernestas-poskus/hyper/commit/6169db250c932dd012d391389826cd34833077b4"
+        },
+        "date": 1633898582720,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "http_connector",
+            "value": 54674,
+            "range": "± 951",
             "unit": "ns/iter"
           }
         ]
