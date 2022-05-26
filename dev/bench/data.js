d4a5d79942ec8fb52231548dce2af3c741378740
github-action-benchmark <github@users.noreply.github.com>
2022-05-26 09:11:12 +0000

    add pipeline (cargo) benchmark result for 4678be9e8100d18bde26d319d8de36dee373bd2f

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1653556248526,
+  "lastUpdate": 1653556272397,
   "repoUrl": "https://github.com/ernestas-poskus/hyper",
   "entries": {
     "pipeline": [
@@ -269,6 +269,36 @@ window.BENCHMARK_DATA = {
             "name": "hello_world_16",
             "value": 49612,
             "range": "± 2112",
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "sean@seanmonstar.com",
+            "name": "Sean McArthur",
+            "username": "seanmonstar"
+          },
+          "committer": {
+            "email": "sean@seanmonstar.com",
+            "name": "Sean McArthur",
+            "username": "seanmonstar"
+          },
+          "distinct": true,
+          "id": "4678be9e8100d18bde26d319d8de36dee373bd2f",
+          "message": "docs(contrib): add guide for Triaging Issues",
+          "timestamp": "2022-05-25T14:13:09-05:00",
+          "tree_id": "2d44870fb37e1f91ee6212007f213c7bebffd9b9",
+          "url": "https://github.com/ernestas-poskus/hyper/commit/4678be9e8100d18bde26d319d8de36dee373bd2f"
+        },
+        "date": 1653556270707,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "hello_world_16",
+            "value": 48768,
+            "range": "± 2221",
             "unit": "ns/iter"
           }
         ]
